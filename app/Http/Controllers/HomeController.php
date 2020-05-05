4a9651db8bd9017ef467fbfcdbdeca0c12f5cbe2
Soundy <qwerthor@gmail.com>
2020-05-06 02:37:59 +0700

    new controller new class

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Lib\ParserPD;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     //
     public function dashboard(){
+        ParserPD::parsePortfolio("somehtmlstring");
+        
         return view('dashboard');
+
+
     }
 }
---
+++ app/Lib/ParserPD.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Lib;
+
+class ParserPD {
+
+    public static function parsePortfolio($html){
+        dd(1);
+    }
+
+    public static function parseGainLoss($html){
+
+    }
+}
